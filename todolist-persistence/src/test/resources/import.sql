0c6d28a584ec3e3cc0d7a775c4229c7274162edf
Meidi <meidi.airouche@gmail.com>
2016-10-19 18:00:59 +0200

    Ajout de la couche de présentation

--- todolist-persistence/src/test/resources/import.sql
+++ todolist-persistence/src/test/resources/import.sql
@@ -15,7 +15,7 @@ INSERT INTO category(name) VALUES('testUpdateCategoryRightNameDB');
 INSERT INTO task(label) VALUES('testFindableLabelDB');
 -- testListTasksForGivenUserAndCategory
 -- ID_USER = 1L
-INSERT INTO user(name, USER_TYPE) VALUES('testUserDB', 'USER_FREE');
+INSERT INTO users(name, USER_TYPE) VALUES('testUserDB', 'USER_FREE');
 -- ID_CATEGORY = 4L
 INSERT INTO category(name) VALUES('testCategoryDB');
 -- ID_TASK = 2L
